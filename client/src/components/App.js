07b3ffd80d221a7490de4fc14158d816f3a453c0
William Choi <wchoi95@gmail.com>
2017-09-16 18:37:50 -0700

    added axiom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.getUsers()
+  }
+
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { console.log(res); })
+    .catch((err) => { console.log(err); })
+  }
+
   render() {
     return (
       <div className="App">
