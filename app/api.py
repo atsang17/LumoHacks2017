988d5192a0ce148983aaba29ef93b62650d14c12
Alexander Tsang <atsang1117@hotmail.com>
2017-09-16 17:27:18 -0700

    fixes to api.py

--- app/api.py
+++ app/api.py
@@ -1,8 +1,6 @@
-from flask import jsonifys
+from flask import jsonify
 from flask_cors import cross_origin
 from app import app
-import requests
-import json
 
 @app.route('/hello', methods=['GET'])
 @cross_origin()
